49c46790bd7caad1426489e20b388bb749b95dcc
Camilo Cañas Jaramillo <casmis123@gmail.com>
2024-03-31 17:45:45 -0500

    feat(add DownloadFromOtherNode): nose

--- grpc-server/server.py
+++ grpc-server/server.py
@@ -3,6 +3,8 @@ import grpc
 import os
 import shutil
 import logging
+from threading import Thread
+import random
 
 from dotenv import load_dotenv
 import httpx
@@ -14,6 +16,46 @@ load_dotenv()
 FILES_LOCATION = os.getenv("FILES_LOCATION")
 MAIN_SERVER_URL = os.getenv("MAIN_SERVER_URL")
 MAX_MESSAGE_LENGTH = 1024 * 1024 * 1024  # 1 GB en bytes
+
+
+def replicate_file(filename: str, chunk_number: int, chunk_data: bytes):
+    logging.info(filename)
+    response = httpx.get(f"{MAIN_SERVER_URL}/get-nodes/")
+    response.raise_for_status()
+    nodes = response.json()
+    logging.info(nodes)
+    min_chunk_size_mb = (len(chunk_data) / 1024) * 2
+    logging.info(min_chunk_size_mb)
+    selected_nodes = [
+        node
+        for node, info in nodes.items()
+        if info["storageCapacityInMb"] >= min_chunk_size_mb and info["online"]
+    ]
+    logging.info(selected_nodes)
+
+    if len(selected_nodes) <= 0:
+        raise Exception("El nodo no existe pa")
+
+    selected_node = random.choice(selected_nodes)
+
+    channel = grpc.insecure_channel(
+        f"{selected_node}:50051",
+        options=[
+            ("grpc.max_send_message_length", MAX_MESSAGE_LENGTH),
+            ("grpc.max_receive_message_length", MAX_MESSAGE_LENGTH),
+        ],
+    )
+    stub = filetransfer_pb2_grpc.FileServiceStub(channel)
+
+    response = stub.DownloadFromOtherNode(
+        filetransfer_pb2.ChunkData(
+            filename=filename, chunk_number=chunk_number, chunk_data=chunk_data
+        )
+    )
+    logging.info(response)
+    logging.info(
+        f"Chunk {chunk_number} enviado, respuesta del servidor: {response.message}"
+    )
 
 
 class FileService(filetransfer_pb2_grpc.FileServiceServicer):
@@ -50,6 +92,12 @@ class FileService(filetransfer_pb2_grpc.FileServiceServicer):
                 grpc.StatusCode.NOT_FOUND,
                 f"Name server Error: {ex}\nresponse:{response.content}",
             )
+
+        replicate_thread = Thread(
+            target=replicate_file,
+            args=(request.filename, request.chunk_number, request.chunk_data),
+        )
+        replicate_thread.start()
         return filetransfer_pb2.TransferStatus(
             success=True,
             message="Chunk recibido exitosamente",
@@ -83,6 +131,44 @@ class FileService(filetransfer_pb2_grpc.FileServiceServicer):
                 grpc.StatusCode.INTERNAL,
                 f"Error reading chunk {request.chunk_number} for file '{request.filename}': {str(e)}",
             )
+
+    def DownloadFromOtherNode(self, request, context):
+        logging.info(
+            f"Recibido chunk {request.chunk_number} del archivo '{request.filename}'"
+        )
+
+        # Crear la carpeta si no existe
+        folder_path = os.path.join(FILES_LOCATION, request.filename)
+        os.makedirs(folder_path, exist_ok=True)
+
+        # Construir la ruta del archivo donde se guardará el chunk
+        chunk_path = os.path.join(folder_path, str(request.chunk_number))
+
+        # Guardar el chunk en el archivo correspondiente
+        with open(chunk_path, "wb") as chunk_file:
+            chunk_file.write(request.chunk_data)
+
+        free_disk = shutil.disk_usage("/").free / (1024**2)
+        response = httpx.post(
+            f"{MAIN_SERVER_URL}/add-chunk/",
+            json={
+                "chunkId": request.chunk_number,
+                "fileName": request.filename,
+                "newFreeStorage": free_disk,
+            },
+        )
+        try:
+            response.raise_for_status()
+        except httpx.HTTPStatusError as ex:
+            context.abort(
+                grpc.StatusCode.NOT_FOUND,
+                f"Name server Error: {ex}\nresponse:{response.content}",
+            )
+        return filetransfer_pb2.TransferStatus(
+            success=True,
+            message="Chunk recibido exitosamente",
+            new_free_storage=free_disk,
+        )
 
 
 def serve():
