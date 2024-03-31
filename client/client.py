e876831e704a94117b022568bd8397396ca0365d
Camilo Cañas Jaramillo <casmis123@gmail.com>
2024-03-31 15:44:49 -0500

    update client

--- client/client.py
+++ client/client.py
@@ -5,33 +5,30 @@ import filetransfer_pb2_grpc
 
 def run():
     # Conectar al servidor
-    channel = grpc.insecure_channel("localhost:50051")
+    channel = grpc.insecure_channel("34.123.168.239:50051")
     stub = filetransfer_pb2_grpc.FileServiceStub(channel)
 
-    filename = "archivo_ejemplo.txt"
-    chunk_size = 1024  # Tamaño de chunk en bytes
+    filename = "Me fui.jpg"
 
     with open(filename, "rb") as file:
         chunk_number = 0
-        while True:
-            data = file.read(chunk_size)
-            if not data:
-                break
-            response = stub.SendChunk(
-                filetransfer_pb2.ChunkData(
-                    filename=filename, chunk_number=chunk_number, chunk_data=data
-                )
+        data = file.read()
+        response = stub.SendChunk(
+            filetransfer_pb2.ChunkData(
+                filename=filename, chunk_number=chunk_number, chunk_data=data
             )
-            print(response)
-            print(
-                f"Chunk {chunk_number} enviado, respuesta del servidor: {response.message}"
-            )
-            chunk_number += 1
+        )
+        print(response)
+        print(
+            f"Chunk {chunk_number} enviado, respuesta del servidor: {response.message}"
+        )
 
     response = stub.RequestChunk(
         filetransfer_pb2.ChunkRequest(filename=filename, chunk_number=chunk_number)
     )
     print(response)
+    with open("Llegue.png", "wb") as f:
+        f.write(response.chunk_data)
 
 
 if __name__ == "__main__":
